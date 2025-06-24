a5589c363176ccdb8ea8a4a9fdc8b7ac60ecaac7
Zack Brown <zackaryzbrown@gmail.com>
2025-06-23 23:56:51 -0600

    Music Basic struct

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -42,12 +42,35 @@ function Home() {
         <p>img</p>
       </section>
 
-      <section className="music" id="music">
-        <h2>Music</h2>
+      <section className="music" id="new-releases">
+        <h2>Latest Releases</h2>
+        <ul>
+          <li>Release 1: Description of release 1.</li>
+          <li>Release 2: Description of release 2.</li>
+          <li>Release 3: Description of release 3.</li>
+        </ul>
+      </section>
+
+      <section className="music" id="top-tracks">
+        <h2>Buy Music</h2>
         <ul>
           <li>Track 1: Description of track 1.</li>
           <li>Track 2: Description of track 2.</li>
           <li>Track 3: Description of track 3.</li>
+          <li>Track 4: Description of track 4.</li>
+          <li>Track 5: Description of track 5.</li>
+          <li>Track 6: Description of track 6.</li>
+          <li>Track 7: Description of track 7.</li>
+          <li>Track 8: Description of track 8.</li>
+          <li>Track 9: Description of track 9.</li>
+          <li>Track 10: Description of track 10.</li>
+          <li>Track 11: Description of track 11.</li>
+          <li>Track 12: Description of track 12.</li>
+          <li>Track 13: Description of track 13.</li>
+          <li>Track 14: Description of track 14.</li>
+          <li>Track 15: Description of track 15.</li>
+          <li>Track 16: Description of track 16.</li>
+          <li>Track 17: Description of track 17.</li>
         </ul>
       </section>
 
