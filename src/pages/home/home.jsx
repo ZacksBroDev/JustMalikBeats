e73e63ee05dd0c283bc0107d894f8025de9cf1db
Zack Brown <zackaryzbrown@gmail.com>
2025-06-29 21:36:52 -0600

    home modifications

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -61,25 +61,24 @@ function Home() {
 
           <section className="music" id="top-tracks">
             <h2>Buy Music</h2>
-            <ul>
-              <li>Track 1: Description of track 1.</li>
-              <li>Track 2: Description of track 2.</li>
-              <li>Track 3: Description of track 3.</li>
-              <li>Track 4: Description of track 4.</li>
-              <li>Track 5: Description of track 5.</li>
-              <li>Track 6: Description of track 6.</li>
-              <li>Track 7: Description of track 7.</li>
-              <li>Track 8: Description of track 8.</li>
-              <li>Track 9: Description of track 9.</li>
-              <li>Track 10: Description of track 10.</li>
-              <li>Track 11: Description of track 11.</li>
-              <li>Track 12: Description of track 12.</li>
-              <li>Track 13: Description of track 13.</li>
-              <li>Track 14: Description of track 14.</li>
-              <li>Track 15: Description of track 15.</li>
-              <li>Track 16: Description of track 16.</li>
-              <li>Track 17: Description of track 17.</li>
-            </ul>
+            <p>Check out our latest beats and instrumentals available for purchase.</p>
+            <div className="music-preview">
+              <div className="track-preview">
+                <h4>Denver Nights</h4>
+                <p>A smooth hip-hop beat inspired by Denver's nightlife - $2.99</p>
+              </div>
+              <div className="track-preview">
+                <h4>Mountain High</h4>
+                <p>High-energy trap beat with Colorado mountain vibes - $3.99</p>
+              </div>
+              <div className="track-preview">
+                <h4>Studio Sessions</h4>
+                <p>Smooth R&B instrumental perfect for late-night sessions - $4.99</p>
+              </div>
+            </div>
+            <div className="music-cta">
+              <Link to="/music" className="music-link">Browse Full Catalog & Purchase</Link>
+            </div>
           </section>
 
           <section className="contact" id="contact">
